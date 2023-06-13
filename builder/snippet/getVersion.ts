d7b12b8968b7d50056fd28cfdf0b3b52995c4367
David GABISON <david.gabison@outlook.com>
2023-06-13 17:36:03 +0200

    refactor: catch some errors

--- builder/snippet/getVersion.ts
+++ builder/snippet/getVersion.ts
@@ -1,13 +1,23 @@
-import fs from 'fs-extra';
+import { fs } from '../util/fs';
 
 import { snippetGetFile } from './getFile';
 
 /** Get the snippet version from snippet code */
-export async function snippetGetVersion (snippetName: string): Promise<string> {
+export async function snippetGetVersion (options: { name: string } | { code: string }): Promise<string> {
+  if (!(options as any).name && !(options as any).code) throw new Error('invalid parameters');
   const versionRE = /^ \* Version:\s*(?<version>\S*)$/um;
-  const file = snippetGetFile(snippetName);
-  const code = await fs.readFile(file, 'utf8');
-  const match = code.match(versionRE);
-  if (match) return match.groups.version;
-  throw new Error(`Unable to find version from this snippet ${file}`);
+
+  let code = '';
+  if ('code' in options) code = options.code;
+  else {
+    const file = snippetGetFile(options.name);
+    code = await fs.readFile(file, 'utf8');
+  }
+
+  if ('string' === typeof code) {
+    const match = code.match(versionRE);
+    if (match?.groups?.version) return match.groups.version;
+  }
+
+  throw new Error(`Unable to find version from this snippet ${JSON.stringify(options)}`);
 }

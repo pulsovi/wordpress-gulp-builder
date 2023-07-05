d6184a29efbda3e8ce1e9da37b6535986fec7895
David GABISON <david.gabison@outlook.com>
2023-07-05 20:32:38 +0200

    feat: add snippetPublishVersion

---
+++ builder/snippet/publishVersion.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+import { snippetGetTitle } from './getTitle';
+import type { SnippetGetTitleOptions } from './getTitle';
+import { snippetGetVersion } from './getVersion';
+import type { SnippetGetVersionOptions } from './getVersion';
+
+type SnippetPublishVersionOptions = SnippetGetTitleOptions & SnippetGetVersionOptions;
+
+/** publish given version as last version of given snippet */
+export function snippetPublishVersion (options: SnippetPublishVersionOptions) {
+  const title = snippetGetTitle(options);
+  const version = snippetGetVersion(options);
+
+  if (!title || !version) {
+    console.log('cannot publish version for this snippet, title of version missing', { options, title, version });
+    return;
+  }
+  return axios({
+    method: 'POST',
+    data: { name: title, version },
+    url: 'http://wp-snippet.marchev.fr/s3HR2pEMg4p4'
+  }).then(result => {
+    console.log('snippetPublishVersion', options, result.data);
+    return result;
+  }, error => {
+    console.log('snippetPublishVersion ERROR', options, error);
+    return null;
+  });
+}

732ce8e3f32c5aeefd5281cea7b3ac42131cbc34
David GABISON <david.gabison@outlook.com>
2023-07-11 15:14:46 +0200

    fix: clone must be done _before_ splitting, not after

--- builder/util/bridge.ts
+++ builder/util/bridge.ts
@@ -2,26 +2,7 @@ import Stream from 'stream';
 
 import { tee } from './tee';
 
-interface BridgeOptions {
-  /**
-   * If data is a File, clone it before split. Cloning avoid modification collision by ref.
-   * @default false
-   */
-  clone?: boolean;
-
-  /**
-   * If `file.contents` is a `Buffer` and `options.contents` is `false`, the `Buffer` reference
-   * will be reused instead of copied.
-   * @default true
-   */
-  contents?: boolean | undefined;
-
-  /**
-   * if `clone` or `options.deep` is `false`, custom attributes will not be cloned deeply.
-   * @default true
-   */
-  deep?: boolean | undefined;
-}
+import { TeeOptions } from './tee';
 
 /**
  * Return a passthrough stream which tee all it's data to the given stream
@@ -32,25 +13,22 @@ interface BridgeOptions {
  * @param cloneOptions If `cloneOptions.clone` is true, all other properties
  *   of it will be followed to `File.clone` method
  */
-export function bridge (stream: NodeJS.ReadWriteStream, cloneOptions: BridgeOptions = {}) {
-  const bridgeStream = tee(stream);
-  const connector = new Stream.Writable({
+export function bridge (stream: NodeJS.ReadWriteStream, cloneOptions: TeeOptions = {}) {
+  const bridgeStream = tee(stream, cloneOptions);
+
+  // redirect all stream output to bridge output
+  stream.pipe(new Stream.Writable({
     objectMode: true,
     write (data, _encoding, cb) {
       try {
-        if (data && 'clone' in data && typeof data.clone === 'function') {
-          const { clone, ...options } = cloneOptions;
-          const shadow = clone ? data.clone(options) : data;
-          bridgeStream.push(shadow);
-        }
-        else bridgeStream.push(data);
+        bridgeStream.push(data);
       } catch (error) {
         cb(error);
         return;
       }
       cb();
     }
-  })
-  stream.pipe(connector);
+  }));
+
   return bridgeStream;
 }
--- builder/util/tee.ts
+++ builder/util/tee.ts
@@ -1,12 +1,44 @@
 import Stream from 'stream';
 
-/** Return a passthrough stream which writes all its data to the given stream */
-export function tee (stream: NodeJS.WritableStream): Stream.Transform {
+export interface TeeOptions {
+  /**
+   * If data is a File, clone it before split. Cloning avoid modification collision by ref.
+   * @default false
+   */
+  clone?: boolean;
+
+  /**
+   * If `file.contents` is a `Buffer` and `options.contents` is `false`, the `Buffer` reference
+   * will be reused instead of copied.
+   * @default true
+   */
+  contents?: boolean | undefined;
+
+  /**
+   * if `clone` or `options.deep` is `false`, custom attributes will not be cloned deeply.
+   * @default true
+   */
+  deep?: boolean | undefined;
+}
+
+/**
+ * Return a passthrough stream which writes all its data to the given stream
+ *
+ * @param stream The target second path stream (or pipeline part)
+ * @param cloneOptions If `cloneOptions.clone` is true, all other properties
+ *   of it will be followed to `File.clone` method
+ */
+export function tee (stream: NodeJS.WritableStream, cloneOptions: TeeOptions = {}): Stream.Transform {
   return new Stream.Transform({
     objectMode: true,
     transform (data, encoding, cb) {
       try {
-        stream.write(data, encoding);
+        if (data && ('clone' in data) && (typeof data.clone === 'function')) {
+          const { clone, ...options } = cloneOptions;
+          const shadow = clone ? data.clone(options) : data;
+          stream.write(shadow, encoding);
+        }
+        else stream.write(data, encoding);
       } catch (error) {
         cb(error);
         return;

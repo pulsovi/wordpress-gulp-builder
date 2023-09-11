861662799333dda23c9edeb6101accff6984a1c4
David GABISON <david.gabison@outlook.com>
2023-09-11 14:56:29 +0200

    debug: add debug logs

--- builder/dev/bindDebugLog.ts
+++ builder/dev/bindDebugLog.ts
@@ -15,21 +15,35 @@ export function bindDebugLog () {
   fs.ensureFile('./debug.log').catch(console.error);
   fs.ensureFile(`${cwd}/debug.log`).catch(console.error);
   const loader = watch('debug.log', { cwd, ignorePermissionErrors: true })
-    .pipe(vinylFilter((data: Vinyl) => Boolean(data.stat!.size)))
-    .pipe(log('Error:', data => {
-      const lines = data.contents!.toString().split('\n') || [''];
-      const message = lines[0].length < 100 ? lines.join('\\n').slice(0, 100) : lines[0];
-      return chalk.red(message) + ' see debug.log file';
-    }))
-    .pipe(dest('.'));
+    .on('error', error => { info('bindDebugLog loader watcher error', error); })
+    .pipe(
+      vinylFilter((data: Vinyl) => Boolean(data.stat!.size))
+      .on('error', error => { info('bindDebugLog loader vinylFilter error', error); })
+    )
+    .pipe(
+      log('Error:', data => {
+        const lines = data.contents!.toString().split('\n') || [''];
+        const message = lines[0].length < 100 ? lines.join('\\n').slice(0, 100) : lines[0];
+        return chalk.red(message) + ' see debug.log file';
+      })
+      .on('error', error => { info('bindDebugLog loader log error', error); })
+    )
+    .pipe(
+      dest('.')
+      .on('error', error => { info('bindDebugLog loader dest error', error); })
+    );
 
   const cleaner = watch('src', { ignorePermissionErrors: true })
-    .pipe(doAction(async (data: Vinyl) => {
-      if (data.event !== 'change') return;
-      if (!(await fs.stat(`${cwd}/debug.log`)).size) return;
-      info(`${data.event} ${chalk.magenta(data.relative)}, truncate debug.log`);
-      await fs.truncate(`${cwd}/debug.log`);
-    }));
+    .on('error', error => { info('bindDebugLog cleaner watcher error', error); })
+    .pipe(
+      doAction(async (data: Vinyl) => {
+        if (data.event !== 'change') return;
+        if (!(await fs.stat(`${cwd}/debug.log`)).size) return;
+        info(`${data.event} ${chalk.magenta(data.relative)}, truncate debug.log`);
+        await fs.truncate(`${cwd}/debug.log`);
+      })
+      .on('error', error => { info('bindDebugLog cleaner doAction error', error); })
+    );
 
   return cleaner;
 }
--- builder/util/fs.ts
+++ builder/util/fs.ts
@@ -1,28 +1,28 @@
 import fsExtra from 'fs-extra';
 
 /** Add error stack to fs async errors */
-export const fs: typeof fsExtra = (() => {
-  const retval: Partial<typeof fsExtra> = {};
-  Object.entries(fsExtra).forEach(([name, func]: [string, any]) => {
-    if (typeof func !== 'function') {
-      retval[name] = func;
-      return;
-    }
-    retval[name as any] = function (...args) {
-      let stack = new Error('').stack ?? '';
-      try {
-        const result = Reflect.apply(func, this, args);
-        if (!(result instanceof Promise)) return result;
+export const fs: typeof fsExtra = (() =>
+  Object.entries(fsExtra).reduce<typeof fsExtra>((fsDebug, [name, func]) => {
+    if (typeof func !== 'function' || name.endsWith('Sync')) {
+      fsDebug[name] = func;
+    } else {
+      fsDebug[name as any] = function (...args) {
+        let stack = new Error('').stack ?? '';
+        try {
+          const result = Reflect.apply(func, this, args);
+          if (!(result instanceof Promise)) return result;
 
-        return result.catch(error => Promise.reject(packError(stack, error)));
-      } catch (error) {
-        // Unable to create new Error here, the call stack is already broken
-        return Promise.reject(packError(stack, error));
+          return result.catch(error => Promise.reject(packError(stack, error)));
+        } catch (error) {
+          // There would be no point in creating `new Error` here. The call stack is already broken.
+          console.info('FS_DEBUG ERROR', { error, args });
+          return Promise.reject(packError(stack, error));
+        }
       }
     }
-  }, {});
-  return retval as typeof fsExtra;
-})();
+    return fsDebug;
+  }, {} as typeof fsExtra)
+)();
 
 function packError (stack: string, error: Error): Error {
   error.stack = error.message + stack.slice(stack.indexOf('\n'));

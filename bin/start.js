84656c4541894a5f13c0be8381c8f22e48596c7f
David GABISON <david.gabison@outlook.com>
2023-10-26 09:38:10 +0200

    refactor: replace binary name to wpbuilder

--- bin/start.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-
-process.argv.push('--cwd', '.', '-f', path.resolve(__dirname, '../dist/gulpfile.js'), 'dev');
-require('gulp-cli')();
---
+++ bin/wpbuilder.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+process.argv.splice(2, 0, '--cwd', '.', '-f', path.resolve(__dirname, '../dist/gulpfile.js'));
+require('gulp-cli')();

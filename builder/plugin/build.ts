fb37b8a04b14b64e0f0e798fb76279fad92b32a3
David GABISON <david.gabison@outlook.com>
2024-03-17 14:25:04 +0100

    fix: non existent main plugin file

--- builder/plugin/build.ts
+++ builder/plugin/build.ts
@@ -5,10 +5,12 @@ import gulp from 'gulp';
 import zip from 'gulp-zip';
 import type Vinyl from 'vinyl';
 
+import { fs } from '../util/fs.js';
 import { log } from '../util/log.js';
 import { pipelineFollowError } from '../util/pipelineFollowError.js';
 import { doAction } from '../util/doAction.js';
 
+import { pluginGetFile } from './getFile.js';
 import { pluginGetVersion } from './getVersion.js';
 import { pluginProcessDoc } from './processDoc.js';
 import { pluginPublishVersion } from './publishVersion.js';
@@ -21,6 +23,11 @@ export function pluginBuild () {
     objectMode: true,
     async write (data: Vinyl, _encoding, cb) {
       const pluginName = data.basename;
+      if (!await fs.exists(pluginGetFile(pluginName))) {
+        log(`Unable to find the main file for the plugin "${pluginName}"`);
+        cb();
+        return;
+      }
       const version = await pluginGetVersion({ async: true, name: pluginName, isRequired: true });
       const displayName = `pluginBuild_${pluginName}`;
       const task = Object.assign(() => pluginBuildTask(pluginName, version), { displayName });

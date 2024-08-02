77add905e7062d369da29010bb941375cd4173fb
David GABISON <david.gabison@outlook.com>
2024-08-02 13:09:31 +0200

    fix: file-by-file test and remove

--- builder/plugin/cleanServerFiles.ts
+++ builder/plugin/cleanServerFiles.ts
@@ -2,17 +2,28 @@ import path from 'path';
 
 import { fs } from '../util/fs.js';
 import { getConfig } from '../util/config.js';
+import gulp from 'gulp';
+import { Stream } from 'stream';
+import type Vinyl from 'vinyl';
+
+const { src } = gulp;
 
 /** Delete from server all plugins managed here */
 export async function pluginsCleanServerFiles (cb) {
   const knownPlugins = await fs.readdir('src/plugins', { withFileTypes: true });
-  const installedPlugins = await fs.readdir(path.join(getConfig().server.root, 'wp-content/plugins'));
-  await Promise.all(knownPlugins
-    .filter(dirent => dirent.isDirectory() && installedPlugins.includes(dirent.name))
-    .map(dirent => fs.rm(
-      path.join(getConfig().server.root, 'wp-content/plugins', dirent.name),
-      { force: true, recursive: true }
-    ))
-  );
+  const serverPluginFolder = path.join(getConfig().server.root, 'wp-content/plugins');
+  const sources = knownPlugins
+    .filter(dirent => dirent.isDirectory())
+    .map(plugin => path.join(getConfig().server.root, 'wp-content/plugins', plugin.name, '**'));
+  return src(sources, { base: serverPluginFolder, read: false })
+    .pipe(new Stream.Writable({
+      objectMode: true,
+      async write (file: Vinyl, __encoding, cb) {
+        if (file.isDirectory()) return cb();
+        if (await fs.exists(path.join('src/plugins', file.relative))) return cb();
+        await fs.unlink(file.path);
+        cb();
+      }
+    }));
 }
 pluginsCleanServerFiles.displayName = 'pluginsCleanServerFiles';

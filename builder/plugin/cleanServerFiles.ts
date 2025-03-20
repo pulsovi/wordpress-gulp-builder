5deb60e611545efab22d28957777bef9775fe853
David GABISON <david.gabison@outlook.com>
2025-03-20 15:52:50 +0100

    refactor: plugin clean server files

--- builder/plugin/cleanServerFiles.ts
+++ builder/plugin/cleanServerFiles.ts
@@ -1,35 +1,40 @@
-import path from 'path';
+import path from 'node:path';
+import { pipeline, Writable } from 'node:stream';
+
+import gulp from 'gulp'; const { src } = gulp;
+import type Vinyl from 'vinyl';
 
 import { fs } from '../util/fs.js';
 import { getConfig } from '../util/config.js';
-import gulp from 'gulp';
-import { Stream } from 'stream';
-import type Vinyl from 'vinyl';
 
-const { src } = gulp;
-
-/** Delete from server all plugins managed here */
-export async function pluginsCleanServerFiles (cb) {
+/**
+ * Gulp task: Delete from server all plugins managed here
+ */
+export async function pluginsCleanServerFiles (taskCb) {
   const knownPlugins = await fs.readdir('src/plugins', { withFileTypes: true });
   const serverPluginFolder = path.join(getConfig().server.root, 'wp-content/plugins');
   const sources = knownPlugins
     .filter(dirent => dirent.isDirectory())
     .map(plugin => path.join(getConfig().server.root, 'wp-content/plugins', plugin.name, '**'));
 
-  if (!sources.length) {
-    cb();
-    return;
-  }
+  if (!sources.length) return taskCb();
 
-  return src(sources, { base: serverPluginFolder, read: false })
-    .pipe(new Stream.Writable({
-      objectMode: true,
-      async write (file: Vinyl, __encoding, cb) {
-        if (file.isDirectory()) return cb();
-        if (await fs.exists(path.join('src/plugins', file.relative))) return cb();
-        await fs.unlink(file.path);
-        cb();
-      }
-    }));
+  return pipeline(
+    src(sources, { base: serverPluginFolder, read: false }),
+    fileUnlinker()
+  );
 }
-pluginsCleanServerFiles.displayName = 'pluginsCleanServerFiles';
+// pluginsCleanServerFiles.displayName = 'pluginsCleanServerFiles';
+
+function fileUnlinker () {
+  return new Writable({
+    objectMode: true,
+    async write (file: Vinyl, _encoding, cb) {
+      if (!await fs.exists(path.join('src/plugins', file.relative))) {
+        if (file.isDirectory()) await fs.rm(file.path, {recursive: true});
+        else await fs.unlink(file.path);
+      }
+      cb();
+    }
+  });
+}
